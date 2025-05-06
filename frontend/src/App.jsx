bde223b653bc62ce9bd9b83f198b04a2c7fbff10
Ayodhya <ayodhyarode03>
2025-05-06 20:46:44 +0530

    React-Router-DOM-installation-and-setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 function App() {
  
 
   return (
-    <div className='text-red-800'>
-      hello
+    <div>
+      <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<Home/>}></Route>
+              <Route path='/login' element={<Login/>}></Route>
+              <Route path='/signup' element={<Signup/>}></Route>
+
+          </Routes>
+      </BrowserRouter>
     </div>
   )
 }
